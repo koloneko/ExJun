77fee575e5decb8dd59788db751dc78dcf4142e2
JC <13724027210@163.com>
2022-10-29 16:45:46 +0800

    字典学习

--- demo03.py
+++ demo03.py
@@ -2,12 +2,17 @@ name = str(input("请输入您的名字："))
 age = int(input("请输入您的年龄："))
 sex = str(input("请输入您的性别："))
 
-a = {"name":name,"age":age,"sex":sex}
+a = {"姓名":name,"age":age,"sex":sex}
 # 输入赋值并以键值对结构存入字典
 
-a.update(name="超")
-# 更新字典里key为name的值
-
-print("您的姓名：",a["name"])
+print("您的姓名：",a["姓名"])
 print("您的年龄：",a["age"])
 print("您的性别：",a["sex"])
+
+a.update(姓名="超")
+# 更新字典里key为姓名的值
+print("更新字典姓名第二次输出",a)
+
+del a["姓名"]
+# 删除字典里key为姓名的值
+print("删除字典姓名第三次输出",a)
